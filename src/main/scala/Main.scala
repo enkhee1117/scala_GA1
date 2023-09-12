d3117ac0cbe7cca81a5dde5f5c25759a5cc02a1f
enkhee1117 <enkhee1117@gmail.com>
2023-09-12 14:45:15 -0500

    problem 1 and 2 solved

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,19 @@
 @main def hello: Unit =
   println("Hello world!")
-  println(msg)
+  println(sum)
+  println(sumUsingMap + " : Sum")
 
-def msg = "I was compiled by Scala 3. :)"
+
+// Problem 1:
+val array = Array(1,2,3,4,5)
+
+// foldLeft method to sum all elements of an array
+val sum = array.foldLeft(0)(_ + _) // this also satisfies the problem 2
+
+// use map method to sum all elements of an array
+val sumUsingMap = array.map(x => x).sum
+
+// Problem 2:
+
+// Problem 3:
+  
